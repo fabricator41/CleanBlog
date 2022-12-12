23829c03ca2b3674a51dfa2a96028c4b415f85f7
Emre Ilhan <emrilhan@gmail.com>
2022-12-12 16:35:56 +0300

    3.kısım

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const ejs = require('ejs');
 const path = require('path');
+const Post = require('./models/Post');
 
 const app = express();
+
+// connect DB
+
+mongoose.connect('mongodb://127.0.0.1:27017/cleanblog-test-db');
+
+// TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // ROUTES
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', { posts });
 });
 
 app.get('/about', (req, res) => {
@@ -23,5 +35,13 @@ app.get('/add_post', (req, res) => {
   res.render('add_post');
 });
 
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
+});
+
 const port = 3000;
-app.listen(port);
+
+app.listen(port, () => {
+  console.log(`sunucu ${port} portunda başlatıldı`);
+});
